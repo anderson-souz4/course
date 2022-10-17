60b6e827fae7538f88d5d1f66493ffc9f7650a9d
anderson-souz4 <andersondesouzalopes69@gmail.com>
2022-10-17 17:10:53 -0300

    Service layer, component registration

--- src/main/java/com/github/course/resources/UserResource.java
+++ src/main/java/com/github/course/resources/UserResource.java
@@ -2,8 +2,11 @@ package com.github.course.resources;
 
 import com.github.course.entities.User;
 import com.github.course.repositories.UserRepository;
+import com.github.course.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,6 +17,22 @@ import java.util.List;
 public class UserResource {
 
     @Autowired
+    UserService service;
+
+    @Autowired
     UserRepository repository;
+
+    @GetMapping
+    public ResponseEntity<List<User>> findAll(){
+        List<User> list = service.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id){
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
+
+    }
 
 }
---
+++ src/main/java/com/github/course/services/UserService.java
@@ -0,0 +1,25 @@
+package com.github.course.services;
+
+
+import com.github.course.entities.User;
+import com.github.course.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> findAll(){
+        return userRepository.findAll();
+    }
+
+    public User findById(Long id){
+        Optional<User> byId = userRepository.findById(id);
+        return byId.get();
+    }
+}
